afd48e74c8b5f1cbedf4e9497d6b2a76b2fda7e9
Marcelo <marcelo.rusconi-ext@dominion-global.com>
2018-05-17 11:06:42 +0200

    Update node and dependencies version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:9.7.1
+FROM node:10.1.0
 
 # Create app directory
 RUN mkdir -p /usr/src/app
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-npm i -D --global gulp
 npm install
+
 npm install -g nodemon
+
 npm run start_dev
